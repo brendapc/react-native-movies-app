192085e12e804ca0055578128c38e265d0815239
Brenda Pereira <brendacamara@outlook.com>
2025-04-08 00:13:39 -0300

    wip: movie details page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View className="bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Text className="text-white">{id}</Text>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+          />
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default MovieDetails
+export default MovieDetails;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- services/api.ts
+++ services/api.ts
@@ -26,3 +26,23 @@ export const fetchMovies = async ({ query }: { query: string }) => {
 
   return data.results;
 };
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}`, {
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch movie details");
+    }
+    const data = await response.json();
+    return data as MovieDetails;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
+};
