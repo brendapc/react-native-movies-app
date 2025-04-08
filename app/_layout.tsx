48112d7cdd080a91b372cec30e12771d8f74f2c6
Brenda Pereira <brendacamara@outlook.com>
2025-04-07 23:11:32 -0300

    feat: hide phone statusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./globals.css";
-import { StyleSheet } from "react-native";
+import { StatusBar } from "react-native";
 import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 };
 
